cc9e40e235920681807ff2cec4633760da516ff9
chayanbansal9195 <chayanbansal1999@gmail.com>
2020-03-28 12:30:27 +0530

    added gitignore procfile and updated ports

--- index.js
+++ index.js
@@ -119,6 +119,11 @@ app.get("/:custom", (req, res) => {
 });
 
 // server connection
-app.listen(3000, function() {
-  console.log("Server connected");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+} 
+
+app.listen(port, function() {
+  console.log("Server connected ");
 });
